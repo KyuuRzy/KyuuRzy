6633d9386c6c5c475416505aacaadd3b8e5b1f23
KyuuRzy Dev <160400183+KyuuRzy@users.noreply.github.com>
2024-06-22 05:55:03 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
     <Layouts>
       <section className="relative flex w-full flex-col items-center justify-evenly py-40 md:h-screen md:flex-row md:overflow-hidden">
         <motion.span {...FadeAnimation} className="title-page">
-          Ramm
+          Kyuu
         </motion.span>
 
         <motion.div {...ContentAnimation} className="relative order-2 flex w-1/2 justify-end md:order-1 md:w-1/3">
@@ -16,14 +16,14 @@ const Home = () => {
           <motion.div {...CardHomeAnimation} animate={{ rotate: -3 }} className="card-home absolute bg-secondary-light" />
           <motion.div {...CardHomeAnimation} animate={{ rotate: 2 }} className="card-home relative bg-primary-light">
             <motion.div {...FadeAnimation} className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-xl">
-              <Image src="/images/profile/profile.png" priority layout="intrinsic" width={500} height={500} alt="Muhamad Rijal" className="relative" />
+              <Image src="/images/profile/profile.png" priority layout="intrinsic" width={500} height={500} alt="KyuuRzy" className="relative" />
             </motion.div>
           </motion.div>
         </motion.div>
 
         <motion.div {...ContentAnimation} className="relative order-1 w-1/3 md:order-2">
           <h3 className="-mb-5 text-base font-normal">Hi! My name is</h3>
-          <h1 className="-ml-1 mb-1 text-7xl font-bold text-primary-light">Ramm</h1>
+          <h1 className="-ml-1 mb-1 text-7xl font-bold text-primary-light">Kyuu</h1>
           <h4 className="text-sm font-light transition-all duration-500 hover:text-primary-light">Front-end Dev</h4>
         </motion.div>
       </section>
